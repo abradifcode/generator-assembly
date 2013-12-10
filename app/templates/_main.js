3c7e8fb7d5ac9752d1b3aa29ad502ad269b78c91
Myke Preuss <myke@signals.ca>
2013-12-10 10:06:11 -0800

    fixing path typo

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -2,11 +2,11 @@ require.config({
 	paths: {
 		jquery: [
 			'//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-			'../bower_compontents/jquery/jquery.min'
+			'../bower_components/jquery/jquery.min'
 		],<% if (includeUnderscore) { %>
 		underscore: [
 			'//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min',
-			'../bower_compontents/underscore/underscore-min'
+			'../bower_components/underscore/underscore-min'
 		],<% } %>
 		app: 'app'
 	},
